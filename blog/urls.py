82547a8b446e871ce5e2a3da5c5d905c87b5c8c0
Jessica Lin <98088800+2680444l@users.noreply.github.com>
2022-07-17 22:50:38 +0100

    project done without styling

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+# django find the first url it finds, so we need to specify the app name
+app_name = 'blog'
+
 urlpatterns = [
     # the default of '' here is the blog folder
     path('', views.all_blogs, name='all_blogs'),
+    # represent the integer as blog ID
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
-
